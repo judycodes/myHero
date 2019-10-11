93615a5a27725ae19ccce364ec51728b4d0c7735
Neil BipatE <neilbipat@gmail.com>
2019-10-10 20:58:54 -0400

    Confirmed the showing of the profile

--- Academia_frontend/js/profile.js
+++ Academia_frontend/js/profile.js
@@ -6,14 +6,15 @@ let token;
 //Profile FORM VARIABLES
 // let email = document.getElementById('update-email');
 let secondEmail = document.getElementById('update-secondEmail');
-let mobile = document.getElementById('update-mobile');
+let updateMobile = document.getElementById('update-mobile');
 // let heroName = document.getElementById('Hero-name');
 // let catchPhrase = document.getElementById('update-catchPhrase')
 //display profile variables
+
 const displayUserName = document.getElementById('username');
 // const displayEmail = document.getElementById('email');
 const displaySecondEmail = document.getElementById('secondEmail');
-const displayMobile = document.getElementById('mobile');
+const displayMobile = document.getElementById('defaultMobile');
 // const displayHeroName = document.getElementById('Heroname');
 // const catchPhrase = document.getElementById('Catchphrase')
 
@@ -34,89 +35,50 @@ function createProfile(e){
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
-        mobile: mobile.value,
+      mobile: updateMobile.value,
       secondary_email: secondEmail.value
-
-      // heroName: heroName.value,
-      // catchPhrase: catchPhrase.value
     })
+
   })
+
   .then((res) => {
     return res.json();
   })
+
   //calls updateProfile function to show new changes from "create/update" profile form
   .then((res) => {
-  console.log(res);
+    console.log(res);
   })
+
   .catch((err) => {
-      console.log(err);
+    console.log(err);
   })
 }
-//   //updates profile function
-// function updateProfile(res) {
-//   //if input value is filled, then don't update form
-//   if(addEmail.value === "" || addEmail.value === null) {
-//     return;
-//   } else {
-//     displayAddEmail.innerHTML = res.additionalEmail;
-//   }
-//   if(mobile.value === "" || mobile.value === null) {
-//     return;
-//   } else {
-//     displayMobile.innerHTML = res.mobile;
-//   }
-//   if(addy.value === "" || mobile.value === null) {
-//     return;
-//   } else {
-//     displayAddy.innerHTML = res.address;
-//   }
-// }
 
+function getProfile(){
+  fetch('http://localhost:8181/profile/', {
+    method: 'GET',
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('user'),
+      "Content-Type": "application/json"
+    }
+  })
 
+  .then((res) => {
+    return res.json();
+  })
 
+  .then((res) => {
+    const mobileInfo = document.createElement('p');
+    mobileInfo.innerText = res.mobile;
+    const secondEmailInfo = document.createElement('p');
+    secondEmailInfo.innerText = res.secondary_email;
+    displaySecondEmail.append(secondEmailInfo);
+    displayMobile.append(mobileInfo);
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
 
-
-// //shows profile information
-// function displayProfile() {
-//   fetch('http://localhost:8181/profile/', {
-//     method: 'GET',
-//     headers: {
-//       "Authorization": "Bearer " + localStorage.getItem('user'),
-//       "Content-Type": "application/json"
-//     }
-//   })
-//   .then((res) => {
-//     return res.json();
-//   })
-//   .then((res) => {
-//     displayUserName.innerHTML = localStorage.getItem('username');
-//     displayPrimaryEmail.innerHTML = localStorage.getItem('primaryEmail');
-//     if(res.additionalEmail === undefined || !localStorage.getItem('secondaryEmail')) {
-//       displayAddEmail.innerHTML = "<em>Add Secondary Email</em>";
-//     } else if(res.additionalEmail) {
-//       displayAddEmail.innerHTML = res.additionalEmail;
-//       localStorage.setItem('secondaryEmail', res.additionalEmail);
-//     } else if (!res.additionalEmail) {
-//       displayAddEmail.innerHTML = localStorage.getItem('secondaryEmail');
-//     }
-//     if(res.mobile === undefined || !localStorage.getItem('mobile')) {
-//       displayMobile.innerHTML = "<em>Add Mobile Number</em>";
-//     } else if(res.mobile) {
-//       displayMobile.innerHTML = res.mobile;
-//       localStorage.setItem('mobile', res.mobile);
-//     } else if(!res.mobile) {
-//       displayMobile.innerHTML = localStorage.getItem('mobile');
-//     }
-//     if(res.address === undefined || !localStorage.getItem('addy')) {
-//       displayAddy.innerHTML = "<em>Add Home Address</em>";
-//     } else if(res.address) {
-//       displayAddy.innerHTML = res.address;
-//       localStorage.setItem('addy', res.address);
-//     } else if(!res.address) {
-//       displayAddy.innerHTML = localStorage.getItem('addy');
-//     }
-// })
-//   .catch((err) => {
-//       console.log(err);
-//   })
-// }
+getProfile();
