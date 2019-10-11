49c1bb7f86d7e2f8596ee645780db766986bb841
Neil BipatE <neilbipat@gmail.com>
2019-10-11 10:08:08 -0400

    updated comments

--- Academia_frontend/js/profile.js
+++ Academia_frontend/js/profile.js
@@ -4,81 +4,95 @@ console.log("js is linked")
 let token;
 
 //Profile FORM VARIABLES
-// let email = document.getElementById('update-email');
 let secondEmail = document.getElementById('update-secondEmail');
 let updateMobile = document.getElementById('update-mobile');
-// let heroName = document.getElementById('Hero-name');
-// let catchPhrase = document.getElementById('update-catchPhrase')
-//display profile variables
 
-const displayUserName = document.getElementById('username');
-// const displayEmail = document.getElementById('email');
+//display profile variables
 const displaySecondEmail = document.getElementById('secondEmail');
 const displayMobile = document.getElementById('defaultMobile');
-// const displayHeroName = document.getElementById('Heroname');
-// const catchPhrase = document.getElementById('Catchphrase')
 
-// displayProfile();
-
-//CREATE PROFILE FUNCTION - used also as a workaround for the update profile
-  //create profile button
+// button used to update and display profile variables
 const createProfileBtn = document.getElementById("update-profile");
 createProfileBtn.addEventListener("click", createProfile);
-  //create profile function
+
+//create profile function
 function createProfile(e){
   e.preventDefault();
-  localStorage.setItem('primaryEmail', email.value);
+
+// The path for the fetch
   fetch('http://localhost:8181/profile/', {
     method: 'POST',
     headers: {
       "Authorization": "Bearer " + localStorage.getItem('user'),
       "Content-Type": "application/json"
     },
+
+// The red represents the variables refered from the model
     body: JSON.stringify({
       mobile: updateMobile.value,
       secondary_email: secondEmail.value
     })
-
   })
 
+// returns the object inside the body
   .then((res) => {
     return res.json();
   })
 
-  //calls updateProfile function to show new changes from "create/update" profile form
+// After the object is returned, the button reloads
   .then((res) => {
-    console.log(res);
-  })
+     window.location.reload();
+   })
 
-  .catch((err) => {
-    console.log(err);
-  })
-}
+// Refers to an error if something goes wrong.
+   .catch((err) => {
+     console.log(err);
+   })
+ }
 
-function getProfile(){
-  fetch('http://localhost:8181/profile/', {
-    method: 'GET',
-    headers: {
-      "Authorization": "Bearer " + localStorage.getItem('user'),
-      "Content-Type": "application/json"
-    }
-  })
+// Will display the variables
+ function getProfile(){
 
-  .then((res) => {
-    return res.json();
-  })
+// The path for the fetch
+   fetch('http://localhost:8181/profile/', {
+     method: 'GET',
+     headers: {
+       "Authorization": "Bearer " + localStorage.getItem('user'),
+       "Content-Type": "application/json"
+     }
+   })
 
-  .then((res) => {
-    const mobileInfo = document.createElement('p');
-    mobileInfo.innerText = res.mobile;
-    const secondEmailInfo = document.createElement('p');
-    secondEmailInfo.innerText = res.secondary_email;
-    displaySecondEmail.append(secondEmailInfo);
-    displayMobile.append(mobileInfo);
-  })
-  .catch(function(error) {
-    console.log(error);
-  });
-}
+// Returns the object from the json body
+   .then((res) => {
+     return res.json();
+   })
 
-getProfile();
+
+   .then((res) => {
+// We are creatingElement for paragraphs where res.mobile will go into
+     const mobileInfo = document.createElement('p');
+// When we get the response it goes into the paragraph
+     mobileInfo.innerText = res.mobile;
+     mobileInfo.classList.add("responseInfo");
+
+// creatingElement for paragraphs where secondary_email will go into
+     const secondEmailInfo = document.createElement('p');
+// When we get the response it goes into the paragraph
+     secondEmailInfo.innerText = res.secondary_email;
+     secondEmailInfo.classList.add("responseInfo");
+
+// Here we append into the disaply varaibles
+     displaySecondEmail.append(secondEmailInfo);
+     displayMobile.append(mobileInfo);
+   })
+
+   .catch(function(error) {
+     console.log(error);
+   });
+ }
+
+// Call the getProfile() function to activate it.
+ getProfile();
+
+
+ 
