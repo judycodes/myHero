b17b4d911735790c13a5a608d63ae0a205a159ba
judycodes <43575215+judycodes@users.noreply.github.com>
2019-10-10 20:47:07 -0400

    view comments functionality, displays comments on frontend

--- Academia_frontend/js/home.js
+++ Academia_frontend/js/home.js
@@ -23,49 +23,56 @@ function listAllPosts () {
   console.log("list posts", res);
   const postsDisplay = document.querySelector('#postsDisplay');
 
-  //loop through post response
+  //loop through posts in database
   for(let i = res.length-1; i >= 0; i--) {
+
 
     const postId = res[i].id;
 
-  //create post related elements
+  //===create post related elements===//
   const postDiv = document.createElement('div');
+  //gives postdiv an id of actual post
+  postDiv.id = `${res[i].id}`;
   const postTitle = document.createElement('h3');
   const postBody = document.createElement('p');
   const postAuthor = document.createElement('span');
+
+    //adds post content to created post title and post body elements
+    postTitle.innerText = res[i].post_title;
+    postBody.innerText = res[i].post_body;
+
+    postAuthor.innerText = `|| author: ${res[i].user.username}`;
+
+  //===delete related elements===//
   const deletePostBtn = document.createElement('button');
 
-  //delete post button styling
-  deletePostBtn.innerText = "delete post";
+    //delete post button styling
+    deletePostBtn.innerText = "delete post";
 
-  //delete post addEventListener
-  deletePostBtn.addEventListener("click", function(event) {
+    //delete post addEventListener
+    deletePostBtn.addEventListener("click", function(event) {
     event.preventDefault();
     deletePost(postId);
     });
 
-  //adds post content to created post title and post body elements
-  postTitle.innerText = res[i].post_title;
-  postBody.innerText = res[i].post_body;
+  //===view comments related elements===//
+  const viewCommentsBtn = document.createElement('button');
 
-  postAuthor.innerText = `|| author: ${res[i].user.username}`;
+    //view comments button styling
+    viewCommentsBtn.innerText = "view comments";
 
-  //create comment related elements
-  const commentDiv = document.createElement('div');
-  const commentBody = document.createElement('p');
+    //view comments addEventListener
+    viewCommentsBtn.addEventListener("click", function(event) {
+      event.preventDefault();
 
-  // incomplete comment display
-  // if(res[i].comments !== []) {
-  //   //adds comment content to create comment elements
-  //   commentBody.innerText = res[i].comments.comment_body;
-  //   commentDiv.append(commentBody);
-  // }
+      viewComments(event);
+    });
 
-  //adds post to each postDiv element
+//adds post to each postDiv element
   postTitle.append(postAuthor);
-  postDiv.append(postTitle, postBody, deletePostBtn);
+  postDiv.append(postTitle, postBody, deletePostBtn, viewCommentsBtn);
 
-  //adds all posts to postsDisplay
+//adds all posts to postsDisplay
   postsDisplay.appendChild(postDiv);
 
   }
@@ -121,7 +128,7 @@ function deletePost(postId) {
     }
   })
   .then((res) => {
-    console.log(res, "res in delete post");
+    //console.log(res, "res in delete post");
     if (res.status == 200) {
       window.location.reload(true);
       alert("Post Was Defeated!");
@@ -135,4 +142,47 @@ function deletePost(postId) {
 }
 
 
-//comments
+//VIEW COMMENTS
+function viewComments(event) {
+  const postId = event.target.parentNode.id;
+  fetch((`http://localhost:8181/post/get-${postId}`), {
+    headers: {
+      "Authorization": "Bearer " + localStorage.getItem('user'),
+      "Content-Type": "application/json"
+    }
+  })
+  .then((res) => {
+    return res.json();
+  })
+
+  .then((res) => {
+    console.log(res, "res in view comments");
+    //console.log(res.comments, "res comments array");
+    //create comment related elements
+//const commentDiv = document.createElement('div');
+
+    const commentsArr = res.comments;
+
+    console.log(commentsArr, "commentsArr");
+    for(let i=0; i < commentsArr.length; i++) {
+      const commentBody = document.createElement('p');
+      commentBody.innerText = commentsArr[i].comment_body;
+      console.log(commentsArr[i], "i commentsArr");
+      event.target.parentNode.append(commentBody);
+    }
+    })
+    .then((error) => {
+      console.log(error);
+    })
+
+
+
+// incomplete comment display
+// if(res[i].comments !== []) {
+//   //adds comment content to create comment elements
+//   commentBody.innerText = res[i].comments.comment_body;
+//   commentDiv.append(commentBody);
+// }
+
+
+}
